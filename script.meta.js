f52eef5f02c272d893938c7a8226b432986f1eac
mazm <marcel.zipaj-misko@student.tuke.sk>
2025-05-09 22:31:23 +0200

    Initial commit

---
+++ script.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Macko Duško
+// @namespace    https://macko.industries.sk/dusan
+// @version      3.65.4
+// @description  Reworked Dob.y2
+// @author       xxxxx and Macko
+// @license      CC BY-NC-ND 4.0; https://creativecommons.org/licenses/by-nc-nd/4.0/
+// @include      https://www.the-west.*
+// @include      https://*.the-west.*/game.php*
+// @include      https://*.the-west.com.*/game.php*
+// @include      https://*.the-west.*.com/game.php*
+// @exclude      https://*.events.the-west.*
+// @exclude      https://*forum.beta.the-west.*
+// @icon         https://westsk.innogamescdn.com/images/items/animal/item_51460.png?7
+// @downloadURL  https://mackodusko.github.io/dusan/script.user.js
+// @updateURL    https://mackodusko.github.io/dusan/script.meta.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_info
+// @grant        unsafeWindow
+// ==/UserScript==
